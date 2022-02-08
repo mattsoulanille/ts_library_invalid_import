9c34f68dfca60d453c04797c24f93490dcf734aa
Matthew Soulanille <matthew@soulanille.net>
2022-02-08 14:38:35 -0800

    Create example of .mjs issue

---
+++ webpack/app.js
@@ -0,0 +1,3 @@
+import {foo} from 'example-package';
+
+console.log(foo);
---
+++ webpack/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mode: 'production',
+  entry: './app.js',
+  target: 'web',
+  output: {
+    filename: 'app_webpack.js',
+  },
+}
